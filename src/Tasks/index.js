169871bbf67e214e00a28984488786fa49e06865
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-07 14:20:29 +0200

    remove unnecessary quote

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -12,7 +12,7 @@ const Tasks = ({ tasks, hideDone }) => (
                     {task.done ? "✔" : ""}
                 </button>
                 <span className=
-                    {`tasks__content${task.done ? " tasks__content--done" : ""}"`}>
+                    {`tasks__content${task.done ? " tasks__content--done" : ""}`}>
                     {task.content}
                 </span>
                 <button className="tasks__button tasks__button--remove">
